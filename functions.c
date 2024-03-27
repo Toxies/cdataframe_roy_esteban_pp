14d47b2d6ccfa92990e662fa30fc1c11ae4e39fb
Toxies <estebanbplaza@gmail.com>
2024-03-27 15:21:19 +0100

    creating first function create_colonne

--- functions.c
+++ functions.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include "functions.h"
 
-struct colonne{
-    char titre;
-    int TP;
-    int TL;
-    int Data[256];
-};
-
-typedef struct colonne COLONNE;
-
 COLONNE *create_colonne(char* titre){
-
+    printf("Hello World");
+    return 0;
 }
--- functions.h
+++ functions.h
@@ -1,1 +1,13 @@
+#ifndef COLONNE_H
+#define COLONNE_H
+struct colonne{
+    char titre;
+    int TP;
+    int TL;
+    int Data[256];
+};
+
+typedef struct colonne COLONNE;
+
+#endif
 COLONNE *create_colonne(char* titre);
