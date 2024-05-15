48ca583f756b17dd5a4b3debc584a10c5e2b3232
Toxies <estebanbplaza@gmail.com>
2024-05-15 16:50:01 +0200

    commit main, testing basic functions

--- main.c
+++ main.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "functions.h"
-#include "functions.c"
 
 int main(){
-    COLONNE *nouvelle_colonne = create_colonne("Colonne 1");
-    printf("%s\n", nouvelle_colonne->titre);
-    insert_value(nouvelle_colonne, 90);
-    printf("%d \n", nouvelle_colonne->Data[0]);
-    insert_value(nouvelle_colonne, 2);
-    printf("%d \n", nouvelle_colonne->Data[1]);
-    print_col(nouvelle_colonne);
-    int a = valeurenposition(10, nouvelle_colonne);
-    printf("%d", a);
     DATAFRAME *dataframe = create_dataframe("Dataframe");
+    COLONNE *nouvelle_colonne = create_colonne("Colonne 1");
+    insert_value(nouvelle_colonne, 90);
+    insert_value(nouvelle_colonne, 2);
     COLONNE  *nouvelle_colonne2 = create_colonne("Colonne 2");
     insert_value(nouvelle_colonne2, 3);
     insert_column(nouvelle_colonne, dataframe);
     insert_column(nouvelle_colonne2, dataframe);
-    printdataframe(dataframe);
     insert_value(dataframe->Data[1], 50);
     printdataframe(dataframe);
-    printf("%d \n", verifyExistence(dataframe, 54));
-    replacevalue(dataframe, 10, 1, 0);
-    printdataframe(dataframe);
-    partialdataframeprint(dataframe, 0, 1);
     return 0;
 };
