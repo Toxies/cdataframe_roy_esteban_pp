b4aa733afd75a7c782e6315eb1bd2311701ca6e2
Royta-creator <126248833+Royta-creator@users.noreply.github.com>
2024-05-20 21:11:08 +0200

    Commentaires dataframe.h

--- dataframe.h
+++ dataframe.h
@@ -3,29 +3,31 @@
 
 #include "colonne.h"
 
+// Structure représentant un nœud de colonne dans le dataframe
 typedef struct noeud_colonne {
     COLONNE *col;
     struct noeud_colonne *precedent;
     struct noeud_colonne *suivant;
 } NOEUD_COLONNE;
 
+// Structure représentant un dataframe
 typedef struct dataframe {
-    NOEUD_COLONNE *tete;
-    NOEUD_COLONNE *queue;
+    NOEUD_COLONNE *tete;   // Pointeur vers le premier premier maillon de la premiere colonne
+    NOEUD_COLONNE *queue;  // Pointeur vers le dernier dernier maillon de la dernieres colonne
 } DATAFRAME;
 
 // Fonctions de gestion de dataframe
-DATAFRAME *creer_dataframe();
-void ajouter_colonne(DATAFRAME *df, COLONNE *col);
-void supprimer_colonne_a(DATAFRAME *df, int index);
-void afficher_dataframe(const DATAFRAME *df);
-void liberer_dataframe(DATAFRAME **df);
-void ajouter_ligne(DATAFRAME *df, int *valeurs);
-void supprimer_ligne(DATAFRAME *df, int index);
-int obtenir_valeur(DATAFRAME *df, int ligne, int colonne);
-void remplacer_valeur_df(DATAFRAME *df, int ligne, int colonne, int nouvelle_valeur);
-int nombre_de_lignes(const DATAFRAME *df);
-int nombre_de_colonnes(const DATAFRAME *df);
-void afficher_noms_colonnes(const DATAFRAME *df);
+DATAFRAME *creer_dataframe();                               // Crée un nouveau dataframe
+void ajouter_colonne(DATAFRAME *df, COLONNE *col);           // Ajoute une colonne au dataframe
+void supprimer_colonne_a(DATAFRAME *df, int index);          // Supprime une colonne du dataframe à un index donné
+void afficher_dataframe(const DATAFRAME *df);                // Affiche les valeurs du dataframe
+void liberer_dataframe(DATAFRAME **df);                      // Libère la mémoire occupée par le dataframe
+void ajouter_ligne(DATAFRAME *df, int *valeurs);             // Ajoute une ligne de valeurs au dataframe
+void supprimer_ligne(DATAFRAME *df, int index);              // Supprime une ligne du dataframe à un index donné
+int obtenir_valeur(DATAFRAME *df, int ligne, int colonne);   // Obtient la valeur à une position donnée dans le dataframe
+void remplacer_valeur_df(DATAFRAME *df, int ligne, int colonne, int nouvelle_valeur); // Remplace une valeur dans le dataframe
+int nombre_de_lignes(const DATAFRAME *df);                   // Renvoie le nombre de lignes du dataframe
+int nombre_de_colonnes(const DATAFRAME *df);                 // Renvoie le nombre de colonnes du dataframe
+void afficher_noms_colonnes(const DATAFRAME *df);            // Affiche les noms des colonnes du dataframe
 
 #endif // DATAFRAME_H
