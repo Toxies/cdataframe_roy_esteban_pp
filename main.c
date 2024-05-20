aee1904a610edd2da8ec25409f5c0b98e15b36ad
Royta-creator <126248833+Royta-creator@users.noreply.github.com>
2024-05-20 22:43:57 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,59 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "dataframe.h"
 
+// Ceci est le fichier principal pour tester les fonctionnalités du dataframe
+
 int main() {
+    // Création du dataframe
     DATAFRAME *df = creer_dataframe();
 
-    COLONNE *col1 = creer_colonne("Colonne 1");
-    ajouter_valeur_colonne(col1, 5);
-    ajouter_valeur_colonne(col1, 10);
-    ajouter_valeur_colonne(col1, 15);
-
-    COLONNE *col2 = creer_colonne("Colonne 2");
-    ajouter_valeur_colonne(col2, 3);
-    ajouter_valeur_colonne(col2, 6);
-    ajouter_valeur_colonne(col2, 9);
-
-    COLONNE *col3 = creer_colonne("Colonne 3");
-    ajouter_valeur_colonne(col3, 1);
-    ajouter_valeur_colonne(col3, 2);
-
+    // Ajout des colonnes
+    COLONNE *col1 = creer_colonne("Colonne1");
+    COLONNE *col2 = creer_colonne("Colonne2");
     ajouter_colonne_dataframe(df, col1);
     ajouter_colonne_dataframe(df, col2);
-    ajouter_colonne_dataframe(df, col3);
 
+    // Ajout des lignes
+    int ligne1[] = {1, 2};
+    int ligne2[] = {3, 4};
+    ajouter_ligne_dataframe(df, ligne1);
+    ajouter_ligne_dataframe(df, ligne2);
+
+    // Affichage du dataframe
     printf("Dataframe initial:\n");
     afficher_dataframe(df);
 
-    printf("Ajout d'une ligne de valeurs:\n");
-    int valeurs_ligne[] = {20, 21, 22};
-    ajouter_ligne_dataframe(df, valeurs_ligne);
-    afficher_dataframe(df);
+    // Affichage limité des lignes
+    printf("\nAfficher les deux premières lignes:\n");
+    afficher_lignes_limite_dataframe(df, 2);
 
-    printf("Suppression de la ligne 2:\n");
-    supprimer_ligne(df, 1);
-    afficher_dataframe(df);
+    // Affichage limité des colonnes
+    printf("\nAfficher les deux premières colonnes:\n");
+    afficher_colonnes_limite_dataframe(df, 2);
 
-    printf("Nombre de lignes: %d\n", nombre_de_lignes(df));
-    printf("Nombre de colonnes: %d\n", nombre_de_colonnes(df));
+    // Test des fonctions pour les valeurs supérieures, inférieures ou égales à 2
+    int x = 2;
+    printf("\nNombre de valeurs supérieures à %d : %d\n", x, nombre_valeurs_sup_a_x(df, x));
+    printf("Nombre de valeurs inférieures à %d : %d\n", x, nombre_valeurs_inf_a_x(df, x));
+    printf("Nombre de valeurs égales à %d : %d\n", x, nombre_valeurs_egales_a_x(df, x));
 
-    printf("Suppression de la colonne 1:\n");
-    supprimer_colonne_a(df, 0);
-    afficher_dataframe(df);
-
-    printf("Renommage de la colonne 2:\n");
-    renommer_colonne(col2, "Nouvelle Colonne 2");
-    afficher_dataframe(df);
-
-    printf("Remplacement de la valeur en (0,0):\n");
-    remplacer_valeur_df(df, 0, 0, 42);
-    afficher_dataframe(df);
-
-    printf("Nombre de valeurs egales a 42 : %d\n", nombre_valeurs_egales_a(col2, 42)+nombre_valeurs_egales_a(col3, 42));
-    printf("Nombre de valeurs superieures a 5 : %d\n", nombre_valeurs_sup_a(col2, 5)+nombre_valeurs_sup_a(col2, 5));
-    printf("Nombre de valeurs inferieures a 10 : %d\n", nombre_valeurs_inf_a(col2, 10)+nombre_valeurs_inf_a(col2, 10));
-
+    // Libération de la mémoire
     liberer_dataframe(&df);
 
     return 0;
